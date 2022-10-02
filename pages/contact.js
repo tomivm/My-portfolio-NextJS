5ef9a594b0b8a6ec815b8486b23c13a88e2a5673
tomivm <tomivm98@gmail.com>
2022-10-01 22:38:59 -0300

    wip

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,55 @@
+import { FaGithub, FaLinkedin, FaWhatsapp, FaDiscord } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import contactStyles from "./contact.module.css";
 export default function Projects() {
   return (
-    <div className="blueBackground">
+    <div
+      className="blueBackground"
+      style={{ minHeight: "100%", paddingBottom: 0 }}
+    >
       <div className="wrapper">
-        <h1 style={{ paddingBottom: "1em", paddingTop: "0.5em" }}>Contact</h1>
-        <div></div>
+        <h1 className={contactStyles.title}>Contact</h1>
+        <div className={contactStyles.linksContainer}>
+          {/* <a
+            aria-label="open Github profile"
+            href="https://github.com/tomivm"
+            target="_blank"
+          >
+            <FaGithub size={50} />
+            <p>Github</p>
+          </a> */}
+          <a
+            aria-label="mail to"
+            href="mailto:tomivm98@gmail.com?Subject=I%20saw%20your%20portfolio"
+          >
+            <IoMdMail size={50} />
+            <p>Email</p>
+          </a>
+          <a
+            aria-label="open Linkedin profile"
+            href="https://www.linkedin.com/in/tomas-van-morlegan-925732179/"
+            target="_blank"
+          >
+            <FaLinkedin size={50} />
+            <p>Linkedin</p>
+          </a>
+          <a
+            aria-label="Whatsapp to"
+            href="https://wa.me/5493517724769"
+            target="_blank"
+          >
+            <FaWhatsapp size={50} />
+            <p>Whatsapp</p>
+          </a>
+          <a
+            aria-label="Discord to"
+            href="https://discord.com/channels/@me/tomivm#2865"
+            target="_blank"
+          >
+            <FaDiscord size={50} />
+            <p>Discord</p>
+          </a>
+        </div>
       </div>
     </div>
   );
